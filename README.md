b8edafcfbac018dee7cb86c191679cd3b915a3cf
Phil Wood <philwood128@hotmail.com>
2016-11-11 11:18:54 +0100

    Start

---
+++ README.md
@@ -0,0 +1,4 @@
+wwwroot
+=======
+
+A Symfony project created on November 9, 2016, 11:40 am.
---
+++ src/Odaku/OdakuBundle/OdakuOdakuBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Odaku\OdakuBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OdakuOdakuBundle extends Bundle
+{
+}
